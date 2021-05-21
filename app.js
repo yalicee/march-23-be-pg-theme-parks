1c0943d6afe54086d1e7ed3464412a4063bd449d
Paul <paulerogerson@gmail.com>
2021-05-21 15:20:47 +0100

    update routing

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
-
-const apiRouter = require('./routes/api');
-
 const app = express();
 
-app.use('/api', apiRouter);
+const {
+  getParks,
+  getParkById,
+  removeParkById,
+  postPark,
+  patchParkById,
+} = require('./controllers/parks');
+
+app.route('/api/parks').get(getParks).post(postPark);
+
+app
+  .route('/api/parks/:park_id')
+  .get(getParkById)
+  .delete(removeParkById)
+  .patch(patchParkById);
+
+app.all('/*', (req, res) => {
+  res.status(404).send({ msg: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.sendStatus(500);
+});
 
 module.exports = app;
--- routes/api.js
+++
@@ -1,6 +0,0 @@
-const apiRouter = require('express').Router();
-const parksRouter = require('./parks');
-
-apiRouter.use('/parks', parksRouter);
-
-module.exports = apiRouter;
